1e4b95dea66fe5b7d20dea75db17e632435d319c
Joey Teng <joey.teng.dev@gmail.com>
2023-04-13 21:54:20 +0100

    feat(profile): implementation for triangle3d

--- profile_implementation.py
+++ profile_implementation.py
@@ -3,9 +3,12 @@ from typing import Any, Callable, NewType, Optional
 
 import jax
 import jax.numpy as jnp
+from jax import lax
 
-from renderer.original_implementation import (Canvas, Colour, Vec2i, line)
+from renderer.original_implementation import (Canvas, Colour, Triangle3D,
+                                              Vec2i, ZBuffer, line, triangle3d)
 from renderer.renderer import line as line_vectorized
+from renderer.renderer import triangle3d as triangle3d_vectorized
 
 jax.config.update('jax_array', True)
 
@@ -99,12 +102,52 @@ def test_line(line_drawer: LineDrawer) -> Canvas:
 def profile_line():
     """Typical result from Colab Standard GPU instance:
 
-    100 loops, best of 5: 16.9 msec per loop
     100 loops, best of 5: 3.2 msec per loop
+    100 loops, best of 5: 16.9 msec per loop
     """
-    profile(test_line, line)
     profile(test_line, line_vectorized)
+    profile(test_line, line)
+
+
+TriangleDrawer = Callable[[Triangle3D, ZBuffer, Canvas, Colour], Canvas]
+
+
+def test_triangle(triangle_drawer: TriangleDrawer) -> Canvas:
+    width: int = 200
+    height: int = 200
+    # width, height since it will be transposed in final step
+    canvas: Canvas = jnp.zeros((width, height, 3))
+    zbuffer: ZBuffer = jnp.zeros((width, height))
+
+    red: Colour = jnp.array((1., .0, .0))
+    white: Colour = jnp.ones(3)
+    green: Colour = jnp.array((.0, 1., .0))
+
+    triangles = [
+        (((10, 70, 1), (50, 160, 1), (70, 80, 1)), red),
+        (((180, 50, 1), (150, 1, 1), (70, 180, 1)), white),
+        (((180, 150, 1), (120, 160, 1), (130, 180, 1)), green),
+    ]
+
+    for _t, colour in triangles:
+        zbuffer, canvas = triangle_drawer(jnp.array(_t), zbuffer, canvas,
+                                          colour)
+
+    canvas = lax.transpose(canvas, (1, 0, 2))
+
+    return canvas
+
+
+def profile_triangle():
+    """Typical result from Colab Standard GPU instance:
+
+    100 loops, best of 5: 4.01 msec per loop
+    10 loops, best of 5: 1.71 sec per loop
+    """
+    profile(test_triangle, triangle3d_vectorized, times=100)
+    profile(test_triangle, triangle3d, times=10)
 
 
 if __name__ == '__main__':
     profile_line()
+    profile_triangle()
