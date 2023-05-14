95b5902da55d63baa51f1df15b35cbb53f912f4c
Joey Teng <joey.teng.dev@gmail.com>
2023-05-14 18:49:16 +0100

    test(smoke_test): add in more complex images

--- smoke_test.py
+++ smoke_test.py
@@ -4,27 +4,27 @@ import matplotlib.pyplot as plt
 
 from renderer.shaders.gouraud import GouraudShader, GouraudExtraVertexInput
 from renderer.pipeline import render
-from renderer.geometry import Camera, to_homogeneous
+from renderer.geometry import Camera
 from renderer.types import Buffers
 from renderer.utils import transpose_for_display
 
-eye = jnp.array((0, 0, 2))
-center = jnp.array((0, 0, 0))
-up = jnp.array((0, 1, 0))
+eye = jnp.array((0., 0, 2))
+center = jnp.array((0., 0, 0))
+up = jnp.array((0., 1, 0))
 
-width = height = 800
-# width = height = 4
+width = 1920
+height = 1080
 lowerbound = jnp.zeros(2, dtype=int)
 dimension = jnp.array((width, height))
-depth = 2
+depth = 255
 
 camera: Camera = Camera.create(
     model_view=Camera.model_view_matrix(eye=eye, centre=center, up=up),
     projection=Camera.perspective_projection_matrix(
         fovy=90.,
         aspect=1.,
-        z_near=-1e-3,
-        z_far=1e-3,
+        z_near=-1.,
+        z_far=1.,
     ),
     viewport=Camera.viewport_matrix(
         lowerbound=lowerbound,
@@ -36,27 +36,32 @@ camera: Camera = Camera.create(
 buffers = Buffers(
     zbuffer=lax.full(
         (width, height),
-        jnp.finfo(jnp.single).min,
+        # jnp.finfo(jnp.single).min,
+        -1.,
     ),
     targets=(lax.full((width, height, 3), 0.), ),
 )
-face_indices = jnp.array(((0, 1, 2), (1, 3, 2)))
+face_indices = jnp.array((
+    (0, 1, 2),
+    (1, 3, 2),
+    (0, 2, 4),
+    (0, 4, 3),
+    (2, 5, 1),
+))
 extra = GouraudExtraVertexInput(
-    position=jnp.array((
-        (0., 0., 0.),
-        (2., 0., 0.),
+    position=jnp.array(((0., 0., 0.), (2., 0., 0.), (0., 1., 0.), (1., 1., 0.),
+                        (-1, -1, 1.), (-2, 0., 0.))),
+    colour=jnp.array((
+        (1., 0., 0.),
         (0., 1., 0.),
+        (0., 0., 1.),
+        (0., 0., 0.),
+        (1., 1., 1.),
         (1., 1., 0.),
     )),
-    colour=jnp.eye(4, 3, dtype=jnp.single),
 )
 
-print("screen space")
-print(camera.to_screen(to_homogeneous(extra.position)))
-
 result = render(camera, GouraudShader, buffers, face_indices, extra)
-# print("render result")
-# print(result)
 
 # show
 fig, axs = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(16, 8))
