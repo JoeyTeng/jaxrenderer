7af9575278f600bcccab8cb8b749f622696ee8bc
Joey Teng <joey.teng.dev@gmail.com>
2023-05-15 16:47:25 +0100

    feat(renderer/shaders/phong): phong shading, interpolate normals per fragment

---
+++ renderer/shaders/phong.py
@@ -0,0 +1,136 @@
+from typing import NamedTuple
+
+import jax
+import jax.lax as lax
+import jax.numpy as jnp
+from jaxtyping import Array, Bool, Float, jaxtyped
+
+from ..geometry import Camera, normalise, to_homogeneous
+from ..shader import ID, MixerOutput, PerFragment, PerVertex, Shader
+from ..types import Colour, LightSource, Texture, Vec2f, Vec3f, Vec4f
+
+jax.config.update('jax_array', True)
+
+
+class PhongTextureExtraInput(NamedTuple):
+    """Extra input for Phong Shader.
+
+    Attributes:
+      - position: in world space, of each vertex.
+      - normal: in world space, of each vertex.
+      - light: parallel light source, shared by all vertices.
+    """
+    position: Float[Array, "vertices 3"]  # in world space
+    normal: Float[Array, "vertices 3"]  # in world space
+    uv: Float[Array, "vertices 2"]  # in texture space
+    light: LightSource
+    texture: Texture
+
+
+class PhongTextureExtraFragmentData(NamedTuple):
+    normal: Vec3f = jnp.array([0.0, 0.0, 0.0])
+    uv: Vec2f = jnp.zeros(2)
+    colour: Colour = jnp.array([0.0, 0.0, 0.0])
+
+
+class PhongTextureExtraMixerOutput(NamedTuple):
+    """When render to only one target, for simplicity."""
+    canvas: Colour
+
+
+class PhongTextureShader(Shader[PhongTextureExtraInput,
+                                PhongTextureExtraFragmentData,
+                                PhongTextureExtraMixerOutput]):
+    """Phong Shading with simple parallel lighting and texture."""
+
+    @staticmethod
+    @jaxtyped
+    @jax.jit
+    def vertex(
+        gl_VertexID: ID,
+        gl_InstanceID: ID,
+        camera: Camera,
+        extra: PhongTextureExtraInput,
+    ) -> tuple[PerVertex, PhongTextureExtraFragmentData]:
+        # Use gl_VertexID to index in `extra` buffer.
+        position: Vec4f = to_homogeneous(extra.position[gl_VertexID])
+        gl_Position: Vec4f = camera.to_clip(position)
+        assert isinstance(gl_Position, Vec4f)
+
+        # assume normal here is in world space. If it is in model space, it
+        # must be transformed by the inverse transpose of the model matrix.
+        # Ref: https://github.com/ssloy/tinyrenderer/wiki/Lesson-5:-Moving-the-camera#transformation-of-normal-vectors
+        normal: Vec3f = (camera.world_to_clip_norm @ jnp.zeros(4).at[:3].set(
+            normalise(extra.normal[gl_VertexID])))[:3]
+        assert isinstance(normal, Vec3f)
+
+        return (
+            PerVertex(gl_Position=gl_Position),
+            PhongTextureExtraFragmentData(
+                normal=normal,
+                uv=extra.uv[gl_VertexID],
+            ),
+        )
+
+    @staticmethod
+    @jaxtyped
+    @jax.jit
+    def fragment(
+        gl_FragCoord: Vec4f,
+        gl_FrontFacing: Bool[Array, ""],
+        gl_PointCoord: Vec2f,
+        varying: PhongTextureExtraFragmentData,
+        extra: PhongTextureExtraInput,
+    ) -> tuple[PerFragment, PhongTextureExtraFragmentData]:
+        built_in: PerFragment = Shader.fragment(
+            gl_FragCoord,
+            gl_FrontFacing,
+            gl_PointCoord,
+            varying,
+            extra,
+        )[0]
+        assert isinstance(built_in, PerFragment)
+
+        uv = lax.round(varying.uv).astype(int)
+        texture_colour: Colour = extra.texture[uv[0], uv[1]]
+
+        # light colour * intensity
+        light_colour: Colour = texture_colour * extra.light.colour * lax.dot(
+            normalise(varying.normal),
+            normalise(extra.light.direction),
+        )
+
+        return (
+            PerFragment(
+                gl_FragDepth=built_in.gl_FragDepth,
+                keeps=jnp.logical_and(built_in.keeps, gl_FrontFacing),
+            ),
+            PhongTextureExtraFragmentData(
+                colour=lax.cond(
+                    (light_colour >= 0).all(),
+                    lambda: texture_colour * light_colour,
+                    lambda: jnp.zeros(3),
+                ),
+                uv=varying.uv,
+                normal=varying.normal,
+            ),
+        )
+
+    @staticmethod
+    @jaxtyped
+    @jax.jit
+    def mix(
+        gl_FragDepth: Float[Array, "primitives"],
+        keeps: Bool[Array, "primitives"],
+        extra: PhongTextureExtraFragmentData,
+    ) -> tuple[MixerOutput, PhongTextureExtraMixerOutput]:
+        mixer_output: MixerOutput
+        extra_output: PhongTextureExtraFragmentData
+        mixer_output, extra_output = Shader.mix(gl_FragDepth, keeps, extra)
+        assert isinstance(mixer_output, MixerOutput)
+        assert isinstance(extra_output, PhongTextureExtraFragmentData)
+
+        return (
+            mixer_output,
+            PhongTextureExtraMixerOutput(canvas=extra_output.colour),
+        )
--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ from renderer.pipeline import render
 from renderer.shaders.gouraud import GouraudExtraInput, GouraudShader
 from renderer.shaders.gouraud_texture import (GouraudTextureExtraInput,
                                               GouraudTextureShader)
+from renderer.shaders.phong import PhongTextureExtraInput, PhongTextureShader
 from renderer.types import Buffers, LightSource, Texture
 from renderer.utils import transpose_for_display
 from test_resources.utils import Model, load_tga, make_model
@@ -132,10 +133,74 @@ def gouraud_shader_with_texture(model: Model, texture: Texture):
     axs[1].imshow(transpose_for_display(result.targets[0]), origin='lower')
 
 
+def phong_shader_with_texture(model: Model, texture: Texture):
+    eye = jnp.array((1, 1, 3.))
+    center = jnp.array((0, 0, 0))
+    up = jnp.array((0, 1, 0))
+
+    width = height = 800
+    lowerbound = jnp.zeros(2, dtype=int)
+    dimension = jnp.array((width, height))
+    depth = 1.
+
+    camera: Camera = Camera.create(
+        model_view=Camera.model_view_matrix(eye=eye, centre=center, up=up),
+        projection=Camera.perspective_projection_matrix(
+            fovy=40.,
+            aspect=1.,
+            z_near=-1.,
+            z_far=1.,
+        ),
+        viewport=Camera.viewport_matrix(
+            lowerbound=lowerbound,
+            dimension=dimension,
+            depth=depth,
+        ),
+    )
+
+    buffers = Buffers(
+        zbuffer=lax.full((width, height), -20.),
+        targets=(lax.full((width, height, 3), 0.), ),
+    )
+    uv = (
+        # reverse along y direction
+        (model.uv[model.faces_uv.reshape((-1, ))].at[:, 1].multiply(-1))
+        # scale to the dimension of texture map
+        * jnp.array(texture.shape[:2])[None, ...])
+    # swap x, y axis
+    texture = transpose_for_display(texture / 255.)
+
+    extra = PhongTextureExtraInput(
+        # flatten so each vertex has its own "extra"
+        position=model.verts[model.faces.reshape((-1, ))],
+        normal=model.norms[model.faces_norm.reshape((-1, ))],
+        uv=uv,
+        light=LightSource(direction=jnp.array((0., 0., 1.))),
+        texture=texture,
+    )
+
+    result = render(
+        camera,
+        PhongTextureShader,
+        buffers,
+        # such that vertex (3i, 3i + 1, 3i + 2) corresponds to primitive i
+        jnp.arange(model.nfaces * 3).reshape((model.nfaces, 3)),
+        extra,
+    )
+
+    # show
+    fig, axs = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(16, 8))
+
+    axs[0].imshow(transpose_for_display(result.zbuffer), origin='lower')
+    axs[1].imshow(transpose_for_display(result.targets[0]), origin='lower')
+
+
 if __name__ == '__main__':
     model = make_model(open('test_resources/obj/african_head.obj').readlines())
-    gouraud_shader_with_simple_light(model)
-    plt.show()
+    # gouraud_shader_with_simple_light(model)
+    # plt.show()
     texture = load_tga('test_resources/tga/african_head_diffuse.tga')
-    gouraud_shader_with_texture(model, texture)
+    # gouraud_shader_with_texture(model, texture)
+    # plt.show()
+    phong_shader_with_texture(model, texture)
     plt.show()
