77222c3379a63a0b093ddabc07e081f72ffe1285
Joey Teng <joey.teng.dev@gmail.com>
2023-05-22 09:19:35 +0100

    feat(renderer/geometry): support embedding with different values, and projection with 0 as w-component

--- renderer/geometry.py
+++ renderer/geometry.py
@@ -467,14 +467,19 @@ def compute_normals(batch_verts: Float[Array, "b 3 3"]) -> Float[Array, "b 3"]:
 
 
 @jaxtyped
-@jax.jit
+@partial(jax.jit, static_argnames=("value", ))
 def to_homogeneous(
-    coordinates: Float[Array, "*batch dim"], ) -> Float[Array, "*batch dim+1"]:
+    coordinates: Float[Array, "*batch dim"],
+    value: Float[Array, "*batch"] = jnp.array(1.),
+) -> Float[Array, "*batch dim+1"]:
     """Transform the coordinates to homogeneous coordinates by append a batch
-        of 1s in the last axis."""
-    ones: Float[Array, "*batch 1"] = jnp.ones(
+        of `value`s (default 1.) in the last axis."""
+    if not isinstance(value, Float[Array, "*batch"]):
+        value = jnp.array(value)
+
+    ones: Float[Array, "*batch 1"] = jnp.broadcast_to(
+        value.astype(jax.dtypes.result_type(coordinates)),
         (*coordinates.shape[:-1], 1),
-        dtype=jax.dtypes.result_type(coordinates),
     )
     homo_coords: Float[Array, "*batch dim+1"] = lax.concatenate(
         (coordinates, ones),
@@ -505,10 +510,15 @@ def to_cartesian(
     """Transform the homogenous coordinates to cartesian coordinates by divide
         every element with the last element on the last axis, then drop them.
 
-    Noted that when a coordinate is 0 and divides by 0, it will produce a nan;
-    for non-zero elements divides by 0, a inf will be produced.
+    When last component is 0, this function just discard the w-component
+    without division.
     """
-    return normalise_homogeneous(coordinates)[..., :-1]
+    return jnp.where(
+        # if w component is 0, just discard it and return.
+        coordinates[..., -1:] == .0,
+        coordinates[..., :-1],
+        normalise_homogeneous(coordinates)[..., :-1],
+    )
 
 
 @jaxtyped
