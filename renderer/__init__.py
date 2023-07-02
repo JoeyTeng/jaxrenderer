95509f5891a03eed843a496a74147722c38f18ca
Joey Teng <joey.teng.dev@gmail.com>
2023-07-02 13:27:36 +0100

    feat(__init__): expose "render" in pipeline.py

--- renderer/__init__.py
+++ renderer/__init__.py
@@ -1,10 +1,11 @@
 from .geometry import (Camera, normalise, quaternion, quaternion_mul,
                        rotation_matrix)
 from .model import Model, ModelObject, batch_models, merge_objects
+from .pipeline import render
 from .renderer import (CameraParameters, LightParameters, Renderer,
                        ShadowParameters)
 from .scene import Scene
 from .shapes.capsule import UpAxis, create_capsule
 from .shapes.cube import create_cube
-from .types import Buffers, Colour, SpecularMap, Texture, Vec3f
+from .types import Buffers, Colour, LightSource, SpecularMap, Texture, Vec3f
 from .utils import build_texture_from_PyTinyrenderer, transpose_for_display
